0b59b9cb1a6995e584053e2739d287c96bcdbe4a
Vxrpenter <110356385+Vxrpenter@users.noreply.github.com>
2024-03-10 16:21:03 +0100

    Made MOTDManager to static utility method

--- src/main/java/dev/vxrp/simplemaintenance/util/MOTDManager.java
+++ src/main/java/dev/vxrp/simplemaintenance/util/MOTDManager.java
@@ -4,12 +4,7 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class MOTDManager  {
-    private final String motd;
-
-    public MOTDManager(String motd) {
-        this.motd = motd;
-    }
-    public Component build() {
+    public static Component motd(String motd) {
         return LegacyComponentSerializer.legacyAmpersand().deserialize(motd.replace("%newline%", "\n"));
     }
 }
